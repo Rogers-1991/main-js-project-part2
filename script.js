e733aba657907500ff2a63725c9cf3b44ce3df83
Colm Rogers <colmrogers91@gmail.com>
2022-06-15 16:22:09 +0200

    fixing 0 placeholder

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 function operate() {
-  let display = document.getElementById('display');
+  let display = document.getElementById("display");
   console.log(display);
-  
-  let buttons = document.getElementsByClassName('button');
-  console.log(buttons); 
-  
+
+  let buttons = document.getElementsByClassName("button");
+  console.log(buttons);
+
   let btnArray = [...buttons];
   console.log(btnArray);
-  
-  btnArray.map(button => {
-    button.addEventListener('click', (e) => {
-      console.log('clicked');
+
+  let inputLen = 0;
+
+  btnArray.map((button) => {
+    button.addEventListener("click", (e) => {
+      console.log("clicked");
       console.log(e);
       console.log(e.target);
       console.log(e.target.innerText);
-      switch(e.target.innerText) {
-        case 'AC':
-          display.innerText = '0';
+      if (inputLen == 0) {
+        display.innerText = "";
+      }
+      switch (e.target.innerText) {
+        case "AC":
+          display.innerText = "0";
+          inputLen = 0;
           break;
-        case '←':
+        case "←":
           if (display.innerText) {
             display.innerText = display.innerText.slice(0, -1);
           }
           break;
-        case '=':
+        case "=":
+          inputLen = 0;
           try {
             display.innerText = eval(display.innerText);
           } catch {
-              display.innerText = "error!";
+            display.innerText = "error!";
           }
           break;
         default:
-            display.innerText += e.target.innerText; 
+          display.innerText += e.target.innerText;
+          inputLen += 1;
       }
     });
   });
 }
 operate();
-
-
-
